6a47067d8e65776b8c02faa6171be624a2fe0191
nsheikh23 <nabi.sheikh23@gmail.com>
2020-11-11 15:58:59 -0800

    Added school route to flask app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, url_for, jsonify
+from markupsafe import escape
 from bson.json_util import dumps
 import aws_controller as db
 
@@ -8,25 +9,32 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/NBA', methods=['GET'])
+@app.route('/NBA')
 def NBA():
-    if request.method == "GET":
-        details = db.get_all("NBA")
-        print(details)
-        for detail in details:
-            var = detail
+    details = db.get_all("NBA")
+    print(details)
+    for detail in details:
+        var = detail
     return render_template('index.html', var=var)
     # return jsonify(aws_controller.get_all("NBA"))
 
-@app.route('/NFL', methods=['GET'])
+@app.route('/NFL')
 def NFL():
-    if request.method == "GET":
-        details = db.get_all("NFL")
-        print(details)
-        for detail in details:
-            var = detail
+    details = db.get_all("NFL")
+    print(details)
+    for detail in details:
+        var = detail
     return render_template('index.html', var=var)
     # return jsonify(aws_controller.get_all("NFL"))
+
+# @app.route('/School/<schoolname>')
+# def profile(schoolname):
+#     return '{}\'s profile'.format(escape(schoolname))
+
+with app.test_request_context():
+    print(url_for('NBA'))
+    print(url_for('NFL'))
+    print(url_for('profile', username="Duke"))
 
 if __name__ == '__main__':
     app.run(debug=True)
