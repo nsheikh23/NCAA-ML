7e2fc31aa15225234a09b21fb7a10e56ebbb52c2
nsheikh23 <nabi.sheikh23@gmail.com>
2020-11-11 17:32:47 -0800

    Added schema for raw tables, need to chg names

---
+++ Resources/schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE NBA (
+    Pk INTEGER,
+    Tm_x VARCHAR,
+    Player VARCHAR,
+    Pos VARCHAR
+);
+
+CREATE TABLE NFL (
+    DraftYear INTEGER,
+    Rnd INTEGER,
+    Pick INTEGER,
+    Tm VARCHAR,
+    Player VARCHAR,
+    Age INTEGER,
+    To INTEGER,
+    AP1 INTEGER,
+    PB INTEGER,
+    St INTEGER,
+    CarAV INTEGER,
+    DrAV INTEGER,
+    G INTEGER,
+    Cmp INTEGER,
+    Att15 INTEGER,
+    Yds16 INTEGER,
+    TD17 INTEGER,
+    Int18 INTEGER,
+    Att19 INTEGER,
+    Yds20 INTEGER,
+    TD21 INTEGER,
+    Rec INTEGER,
+    Yds23 INTEGER,
+    TD24 INTEGER,
+    Solo INTEGER,
+    Int26 INTEGER,
+    Sk FLOAT,
+    School VARCHAR
+);
