a285a2be36113b5979c3fa5975360081d8345346
nsheikh23 <nabi.sheikh23@gmail.com>
2020-11-13 11:27:22 -0800

    Added bball stats schema

--- static/Resources/schema.sql
+++ static/Resources/schema.sql
@@ -1,8 +1,33 @@
-CREATE TABLE NBA (
-    Pk INTEGER,
-    Tm_x VARCHAR,
+CREATE TABLE NBAdraft (
+    Pick INTEGER,
     Player VARCHAR,
-    Pos VARCHAR
+    Conference VARCHAR,
+    College VARCHAR
+);
+
+CREATE TABLE BballStats (
+    Player VARCHAR,
+    Team VARCHAR,
+    GP INTEGER,
+    MPG FLOAT,
+    FGM FLOAT,
+    FGA FLOAT,
+    FG_pct FLOAT,
+    ThreePM FLOAT,
+    ThreePA FLOAT,
+    Three_pct FLOAT,
+    FTM FLOAT,
+    FTA FLOAT,
+    FT_pct FLOAT,
+    TOV FLOAT,
+    PF FLOAT,
+    ORB FLOAT,
+    DRB FLOAT,
+    RPG FLOAT,
+    APG FLOAT,
+    SPG FLOAT,
+    BPG FLOAT,
+    PPG FLOAT
 );
 
 CREATE TABLE NFL (
