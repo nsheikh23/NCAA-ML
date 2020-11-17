e9ec31074679250b54685fd0d7ebbc604bf7e5f4
nsheikh23 <nabi.sheikh23@gmail.com>
2020-11-17 11:48:39 -0800

    Reorganized repo and flask with Anthony's repo

---
+++ FlaskAppAML/views.py
@@ -0,0 +1,157 @@
+"""
+Routes and views for the flask application.
+"""
+
+from flask import render_template, request, url_for
+from FlaskAppAML import app
+from FlaskAppAML.forms import SubmissionForm
+from datetime import datetime
+import json
+import urllib.request
+import os
+import rdsconnection as db
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    # details = db.get_all()
+    # # print(details)
+    # for detail in details:
+    #     var = detail
+
+    form = SubmissionForm(request.form)
+
+    # Form has been submitted
+    if request.method == 'POST' and form.validate():
+
+        # Plug in the data into a dictionary object 
+        #  - data from the input form
+        #  - text data must be converted to lowercase
+        data =  {
+              "Inputs": {
+                "input1": {
+                  "ColumnNames": ["gender", "age", "size", "weight"],
+                  "Values": [ [
+                      0,
+                      1,
+                      form.title.data.lower(),
+                      0
+
+                    ]
+                  ]
+                }
+              },
+              "GlobalParameters": {}
+            }
+
+        # Serialize the input data into json string
+        body = str.encode(json.dumps(data))
+    
+    return render_template("index.html")
+
+@app.route('/basketball')
+def basketball():
+    bball_key = os.environ.get('API_KEY', "3ykY3j9WZDYvS0Dvf5VoJ1kA0yVT5HVzT+foY4SzKvD6LJhHoysBjlEQWaOniNQCGqsjKrytONq1kdxEWo3Scg==")
+    bball_url = os.environ.get('URL', "https://ussouthcentral.services.azureml.net/workspaces/91af20abfc58455182eaaa615d581c59/services/da7cdb9359a443f0abdef36d30ce8f1c/execute?api-version=2.0&details=true")
+    
+    B_HEADERS = {'Content-Type':'application/json', 'Authorization':('Bearer '+ bball_key)}
+
+    req = urllib.request.Request(bball_url, body, B_HEADERS)
+
+    # Send this request to the AML service and render the results on page
+    try:
+        # response = requests.post(URL, headers=HEADERS, data=body)
+        response = urllib.request.urlopen(req)
+        #print(response)
+        respdata = response.read()
+        result = json.loads(str(respdata, 'utf-8'))
+        result = do_something_pretty(result)
+        # result = json.dumps(result, indent=4, sort_keys=True)
+        return render_template(
+            'basketball.html',
+            title="This is the result from AzureML running our Basketball Conference Prediction:",
+            result=result)
+
+    # An HTTP error
+    except urllib.error.HTTPError as err:
+        result="The request failed with status code: " + str(err.code)
+        return render_template(
+            'basketball.html',
+            title='There was an error',
+            result=result)
+        #print(err)
+
+    return render_template(
+        'basketball.html',
+        form = form,
+        title = 'Run App',
+        time=datetime.now(),
+        message = 'Which conference should you go for?'
+    )
+
+@app.route('/football')
+def football():
+    fball_key = os.environ.get('API_KEY', "3ykY3j9WZDYvS0Dvf5VoJ1kA0yVT5HVzT+foY4SzKvD6LJhHoysBjlEQWaOniNQCGqsjKrytONq1kdxEWo3Scg==")
+    fball_url = os.environ.get('URL', "https://ussouthcentral.services.azureml.net/workspaces/91af20abfc58455182eaaa615d581c59/services/da7cdb9359a443f0abdef36d30ce8f1c/execute?api-version=2.0&details=true")
+
+    F_HEADERS = {'Content-Type':'application/json', 'Authorization':('Bearer '+ fball_key)}
+
+    req = urllib.request.Request(fball_url, body, F_HEADERS)
+
+    # Send this request to the AML service and render the results on page
+    try:
+        # response = requests.post(URL, headers=HEADERS, data=body)
+        response = urllib.request.urlopen(req)
+        #print(response)
+        respdata = response.read()
+        result = json.loads(str(respdata, 'utf-8'))
+        result = do_something_pretty(result)
+        # result = json.dumps(result, indent=4, sort_keys=True)
+        return render_template(
+            'football.html',
+            title="This is the result from AzureML running our Football Conference Prediction:",
+            result=result)
+
+    # An HTTP error
+    except urllib.error.HTTPError as err:
+        result="The request failed with status code: " + str(err.code)
+        return render_template(
+            'football.html',
+            title='There was an error',
+            result=result)
+        #print(err)
+
+    return render_template(
+        'football.html',
+        form = form,
+        title = 'Run App',
+        time=datetime.now(),
+        message = 'Which conference should you go for?'
+    )
+
+def do_something_pretty(jsondata):
+    """We want to process the AML json result to be more human readable and understandable"""
+    import itertools # for flattening a list of tuples below
+
+    # We only want the first array from the array of arrays under "Value" 
+    # - it's cluster assignment and distances from all centroid centers from k-means model
+    value = jsondata["Results"]["output1"]["value"]["Values"][0]
+    #valuelen = len(value)
+    print(value)
+    # Convert values (a list) to a list of tuples [(cluster#,distance),...]
+    # valuetuple = list(zip(range(valuelen-1), value[1:(valuelen)]))
+    # Convert the list of tuples to one long list (flatten it)
+    # valuelist = list(itertools.chain(*valuetuple))
+
+    # Convert to a tuple for the list
+    # data = tuple(list(value[0]) + valuelist)
+
+    # Build a placeholder for the cluster#,distance values
+    #repstr = '<tr><td>%d</td><td>%s</td></tr>' * (valuelen-1)
+    # print(repstr)
+    output='For a brain with the size of : '+value[2]+ "<br/>Our Algorithm would calculate the weight to be: "+ value[4]
+    # Build the entire html table for the results data representation
+    #tablestr = 'Cluster assignment: %s<br><br><table border="1"><tr><th>Cluster</th><th>Distance From Center</th></tr>'+ repstr + "</table>"
+    #return tablestr % data
+    return output
+
--- app.py
+++ app.py
@@ -1,55 +1,14 @@
-from flask import Flask, render_template, request, url_for
-from flask_bootstrap import Bootstrap
-from FlaskAPPAML import app
-import json
-import urllib.request
-import os
-import rdsconnection as db
+"""
+This script runs the FlaskAppAML application using a development server.
+"""
 
-app = Flask(__name__, template_folder="templates")
-bootstrap = Bootstrap(app)
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:group1final@ncaa-athletics.cr5bt5kg46tf.us-west-1.rds.amazonaws.com/NCAA_Athletics"
-# db = SQLAlchemy(app)
-
-@app.route('/')
-def index():
-    details = db.get_all()
-    # print(details)
-    for detail in details:
-        var = detail
-    return render_template("index.html")
-
-@app.route('/basketball')
-def basketball():
-    bball_key = os.environ.get('API_KEY', "3ykY3j9WZDYvS0Dvf5VoJ1kA0yVT5HVzT+foY4SzKvD6LJhHoysBjlEQWaOniNQCGqsjKrytONq1kdxEWo3Scg==")
-    bball_url = os.environ.get('URL', "https://ussouthcentral.services.azureml.net/workspaces/91af20abfc58455182eaaa615d581c59/services/da7cdb9359a443f0abdef36d30ce8f1c/execute?api-version=2.0&details=true")
-    
-    B_HEADERS = {'Content-Type':'application/json', 'Authorization':('Bearer '+ bball_key)}
-
-    return render_template(
-        'basketball.html',
-        basketball_form = basketball_form,
-        title = 'Basketball Predictions',
-        message = 'Which conference should you go for?'
-    )
-
-@app.route('/football')
-def football():
-    fball_key = os.environ.get('API_KEY', "3ykY3j9WZDYvS0Dvf5VoJ1kA0yVT5HVzT+foY4SzKvD6LJhHoysBjlEQWaOniNQCGqsjKrytONq1kdxEWo3Scg==")
-    fball_url = os.environ.get('URL', "https://ussouthcentral.services.azureml.net/workspaces/91af20abfc58455182eaaa615d581c59/services/da7cdb9359a443f0abdef36d30ce8f1c/execute?api-version=2.0&details=true")
-
-    F_HEADERS = {'Content-Type':'application/json', 'Authorization':('Bearer '+ fball_key)}
-
-    return render_template(
-        'football.html',
-        football_form = football_form,
-        title = "Football Predictions",
-        message = "Which conference should you go for?"
-    )
-
-with app.test_request_context():
-    print(url_for('basketball'))
-    print(url_for('football'))
+from os import environ
+from FlaskAppAML import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
